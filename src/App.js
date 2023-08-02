4693d23358ca205afd8ebe9dff6a0b75e2c7c63f
sankarshan <sankarshan.mishra@vitstudent.ac.in>
2023-08-03 00:38:52 +0530

    Commit Successfull

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+      <div className="h-screen">&nbsp;</div>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
